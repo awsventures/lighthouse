d8cf20589e249047da43c4724d46b353f5833e75
Jack Franklin <jacktfranklin@google.com>
2025-04-24 09:58:23 +0100

    Add Insights/Audits toggle banner to report
    Adds a more prominent banner that can be clicked to toggle the view. We
    do not store this setting locally at all, so by default any future
    report will show audits by default.
    
    Feedback welcome in particular as this is my first patch into LH. I know
    that all the other SVG icons are done as background-images but in this
    case I needed to control the fill color, hence why I inlined it.
    
    The exact text is still TBC, but I wanted to get this up now for initial
    review.

--- report/renderer/performance-category-renderer.js
+++ report/renderer/performance-category-renderer.js
@@ -201,6 +201,35 @@ export class PerformanceCategoryRenderer extends CategoryRenderer {
       filmstripEl && timelineEl.append(filmstripEl);
     }
 
+    // Insights / Audits toggle.
+    const container = this.dom.createChildOf(element, 'div', 'lh-perf-insights-toggle');
+
+    const text = 'In 2025 insights will expand and replace audits, let us know your feedback';
+    const textNode = this.dom.createTextNode(text);
+    const textSpan = this.dom.createChildOf(container, 'span', 'lh-perf-toggle-text');
+    const icon = this.dom.createElement('span', 'lh-perf-insights-icon');
+    icon.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M18 13V11H22V13H18ZM19.2 20L16 17.6L17.2 16L20.4 18.4L19.2 20ZM17.2 8L16 6.4L19.2 4L20.4 5.6L17.2 8ZM5 19V15H4C3.45 15 2.975 14.8083 2.575 14.425C2.19167 14.025 2 13.55 2 13V11C2 10.45 2.19167 9.98333 2.575 9.6C2.975 9.2 3.45 9 4 9H8L13 6V18L8 15H7V19H5ZM11 14.45V9.55L8.55 11H4V13H8.55L11 14.45ZM14 15.35V8.65C14.45 9.05 14.8083 9.54167 15.075 10.125C15.3583 10.6917 15.5 11.3167 15.5 12C15.5 12.6833 15.3583 13.3167 15.075 13.9C14.8083 14.4667 14.45 14.95 14 15.35Z"/></svg>';
+    textSpan.appendChild(icon);
+    textSpan.appendChild(textNode);
+
+    const buttonClasses = 'lh-button';
+    const button = this.dom.createChildOf(container, 'button', buttonClasses);
+    // By default the "old" audits are visible.
+    let auditsVisible = true;
+
+    button.innerText = 'Try insights';
+    button.addEventListener('click', event => {
+      event.preventDefault();
+      const swappableSection = this.dom.maybeFind('.lh-perf-audits--swappable');
+      if (swappableSection) {
+        this.dom.swapSectionIfPossible(swappableSection);
+      }
+      auditsVisible = !auditsVisible;
+      button.innerText = auditsVisible ? 'Try insights' : 'Go back to audits';
+    });
+
+    container.appendChild(button);
+
     const legacyAuditsSection =
       this.renderFilterableSection(category, groups, ['diagnostics'], metricAudits);
     legacyAuditsSection?.classList.add('lh-perf-audits--swappable', 'lh-perf-audits--legacy');
